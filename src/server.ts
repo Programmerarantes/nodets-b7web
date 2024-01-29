22cfda220e0384421ab79fdf38482febfa1bc942
Matheus <125316650+Programmerarantes@users.noreply.github.com>
2024-01-29 17:11:03 -0300

    base criada

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mustache from 'mustache-express'
+import path from 'path'
+import mainRoutes from './routes/index'
+
+dotenv.config();
+
+const app = express();
+
+app.set('view engine', 'mustache');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('mustache', mustache());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+//ROTAS
+
+app.use(mainRoutes)
+
+app.use((req, res) => {
+    res.send('página não encontrada!')
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
